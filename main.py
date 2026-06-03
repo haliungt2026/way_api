9576bfc41393aa0219682711d05cd2d596a8d837
haliungt2026 <haliungt@gmail.com>
2026-06-03 21:28:42 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+   return {"message": "Hello FastAPI"}
+
+@app.get("/users")
+def get_users():
+   return [
+       {"id": 1, "name": "Bold"},
+       {"id": 2, "name": "Saraa"}
+   ]
+
+
+
+@app.post("/users")
+def create_user():
+  return {
+      "message": "User created"
+  }
